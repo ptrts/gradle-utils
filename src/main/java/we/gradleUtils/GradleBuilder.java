28cad2b5a95dbfc724876610a097625020beb1d4
Pavel Taruts <ptrtss@gmail.com>
2022-04-05 12:46:55 +0300

    initial commit

---
+++ src/main/java/we/gradleUtils/GradleBuilder.java
@@ -0,0 +1,34 @@
+package we.gradleUtils;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
+import we.processUtils.ProcessRunner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradleBuilder {
+
+    public static void buildGradleProject(File directory) {
+
+        List<String> commandsList = new ArrayList<>();
+
+        String fileName;
+        if (SystemUtils.IS_OS_WINDOWS) {
+            fileName = "gradlew.bat";
+        } else {
+            commandsList.add("sh");
+            fileName = "gradlew";
+        }
+
+        File gradleWrapperFile = FileUtils.getFile(directory, fileName);
+        commandsList.add(gradleWrapperFile.getAbsolutePath());
+
+        commandsList.add("classes");
+
+        String[] commandsArray = commandsList.toArray(String[]::new);
+
+        ProcessRunner.runProcess(directory, commandsArray);
+    }
+}
